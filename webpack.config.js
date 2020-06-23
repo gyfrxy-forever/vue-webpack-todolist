2d12a5138e1a4acbda0efd8e02438e738069df89
gyf <temp@1.com>
2020-06-23 16:41:19 +0800

    webpack配置css单独分离打包; webpack区分打包类库代码及hash优化

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,17 @@
 const path = require('path');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
-// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HTMLPlugin = require('html-webpack-plugin');
 
 const isDev = process.env.NODE_ENV === 'development';
 
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 const config = {
     entry: './src/index.js',  // 入口文件
     output: {  
-        filename: 'bundle.js',
+        filename: 'bundle.[hash:8].js',
         path: path.resolve(__dirname, 'dist')
     },
     module: {
@@ -21,21 +24,6 @@ const config = {
                 test: /\.jsx$/,
                 loader: 'babel-loader'
             },
-            // stylus-loader处理stylus文件,解析成css文件，交给css-loader，然后再使用style-loader插入到DOM，一层一层向上传递
-            {
-                test: /\.styl/,
-                use: [
-                    'vue-style-loader',
-                    'css-loader',
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            sourceMap: true
-                        }
-                    },
-                    'stylus-loader'
-                ]
-            },
             {
                 test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
                 loader: 'url-loader',
@@ -47,16 +35,45 @@ const config = {
         ]
     },
     plugins: [
-        // new CleanWebpackPlugin(),
+        new CleanWebpackPlugin(),
 
         // 请确保引入这个插件！
         new VueLoaderPlugin(),
-        new HTMLPlugin()
-    ]
+        new HTMLPlugin(),
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // all options are optional
+            filename: '[name].css',
+            chunkFilename: '[id].css',
+            ignoreOrder: false, // Enable to remove warnings about conflicting order
+        })
+    ],
+    optimization: {
+        splitChunks: {
+          chunks (chunk) {
+            // exclude `my-excluded-chunk`
+            return chunk.name !== 'my-excluded-chunk';
+          }
+        }
+    }
 };
 
 if(isDev) {
     config.mode = 'development';
+    config.module.rules.push({
+        //css预处理器，使用模块化的方式写css代码
+        //stylus-loader专门用来处理stylus文件，处理完成后变成css文件，交给css-loader.webpack的loader就是这样一级一级向上传递，每一层loader只处理自己关心的部分
+        test: /\.styl/,
+        use: [
+            'vue-style-loader',
+            'css-loader',
+            { 
+                loader: 'postcss-loader', 
+                options: { sourceMap: true } 
+            },
+            'stylus-loader'
+        ]  
+    });
     config.devServer = {
         port: '8000',
         host: '0.0.0.0', 
@@ -67,5 +84,39 @@ if(isDev) {
     }
 }else {
     config.mode = 'production';
+    config.output.filename = '[name].[chunkhash:8].js';
+    config.module.rules.push(
+        //css预处理器，使用模块化的方式写css代码
+        //stylus-loader专门用来处理stylus文件，处理完成后变成css文件，交给css-loader.webpack的loader就是这样一级一级向上传递，每一层loader只处理自己关心的部分
+        {
+            test: /\.styl/,
+            use: [
+                {
+                    loader: MiniCssExtractPlugin.loader,
+                    options: {
+                        // you can specify a publicPath here
+                        // by default it uses publicPath in webpackOptions.output
+                        publicPath: './',
+                        hmr: process.env.NODE_ENV === 'development',
+                    },
+                },
+                'css-loader',
+                { 
+                    loader: 'postcss-loader', 
+                    options: { sourceMap: true } 
+                },
+                'stylus-loader'
+            ]
+        },
+    );
+    config.plugins.push(
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // all options are optional
+            filename: 'styles.[chunkhash].[name].css',
+            chunkFilename: '[id].css',
+            ignoreOrder: false, // Enable to remove warnings about conflicting order
+        })
+    );
 }
 module.exports = config;
