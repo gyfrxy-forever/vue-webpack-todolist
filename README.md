283bde28b03527d8b0a63caa96dc4fd8ce534c30
gyf <temp@1.com>
2020-06-23 14:44:48 +0800

    配置vue的jsx写法以及postcss

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ vue+webpack 实现的todo-list demo
 ### 项目框架
 vue@2.6.11
 
-webpack#4.43.0
+webpack@4.43.0
 
 ### 启动
 + 安装项目依赖 `npm install`
---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+const autoprefixer = required('autoprefixer');  // 自动加浏览器前缀
+
+module.exports = {
+    plugins: [
+        autoprefixer()
+    ]
+}
